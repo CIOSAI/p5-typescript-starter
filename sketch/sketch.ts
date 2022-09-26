246a29fcd0846aaaf28708b198fbb3c076ccd97e
CIOSAI <jyinteractive.tw@gmail.com>
2022-09-27 00:02:01 +0800

    more set up

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,47 +1,31 @@
-// GLOBAL VARS & TYPES
-let numberOfShapesControl: p5.Element;
+var sketch = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 
-// P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
-function setup() {
-  console.log("🚀 - Setup initialized - P5 is running");
+  p.draw = () => {
+    p.clear();
+    p.fill(40);
+    p.rect(20, 40, 50, 50);
+  };
+};
+new p5(sketch);
 
-  createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER).noFill().frameRate(30);
-  // NUMBER OF SHAPES SLIDER
-  numberOfShapesControl = createSlider(1, 30, 15, 1).position(10, 10).style("width", "100px");
-}
+var sketch2 = (p: p5) => {
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+  };
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  };
 
-// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
-
-// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
-function draw() {
-  
-   // CLEAR BACKGROUND
-  background(0);
-
-  // CENTER OF SCREEN
-  translate(width / 2,height / 2);
-
-  const numberOfShapes = <number>numberOfShapesControl.value();
-  const colours = ColorHelper.getColorsArray(numberOfShapes);
-
-  // CONSISTENT SPEED REGARDLESS OF FRAMERATE
-  const speed = (frameCount / (numberOfShapes * 30)) * 2;
-  
-  // DRAW ALL SHAPES
-  for (var i = 0; i < numberOfShapes; i++) {
-    push();
-      const lineWidth = 8;
-      const spin = speed * (numberOfShapes - i);
-      const numberOfSides = 3 + i;
-      const width = 40 * i;
-      strokeWeight(lineWidth); 
-      stroke(colours[i]);
-      rotate(spin);
-      PolygonHelper.draw(numberOfSides, width)
-    pop();
-  }
-}
+  p.draw = () => {
+    p.clear();
+    p.fill(200, 40, 40);
+    p.rect(60, 40, 50, 50);
+  };
+};
+new p5(sketch2);
